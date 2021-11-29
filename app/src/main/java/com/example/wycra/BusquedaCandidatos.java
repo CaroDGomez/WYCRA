7bcaf011b4c34403335cd6a81724141f87350e71
CaroDGomez <caro5952@gmail.com>
2021-11-29 14:58:33 -0500

    creación repositorio y vistas iniciales

---
+++ app/src/main/java/com/example/wycra/BusquedaCandidatos.java
@@ -0,0 +1,14 @@
+package com.example.wycra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BusquedaCandidatos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_busqueda_candidatos);
+    }
+}
---
+++ app/src/main/java/com/example/wycra/EstadisticasCandidatos.java
@@ -0,0 +1,14 @@
+package com.example.wycra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EstadisticasCandidatos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estadisticas_candidatos);
+    }
+}
